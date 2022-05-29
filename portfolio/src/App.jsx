e3fc20dd349649bd7f1a1a8e9b550c678e80490b
Daniel Custodio <play.daniel@hotmail.com>
2022-05-28 23:51:18 -0300

    Alterando o projeto para React

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sections from "./gridSections/Sections";
+
+function App() {
+  return (
+    <main>
+      <Sections />
+    </main>
+  );
+}
+
+export default App;
